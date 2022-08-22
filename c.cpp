fad6810ca3dd437430946e417359f9ade0991ecf
fishBone000 <FishBoneEK@outlook.com>
2022-08-22 15:20:45 +0000

    New algorithm for Problem C

--- c.cpp
+++ c.cpp
@@ -3,142 +3,55 @@
 
 using namespace std;
 
-class Undigraph{
-	private:
-		unsigned char * matrix;
-		int n;
-
-	public:
-		Undigraph(int n){
-			this->n = n;
-			matrix = new unsigned char[(1+n)*n/16+1]{0};
-		}
-
-		void release(){
-			delete matrix;
-		}
-
-		Undigraph operator*(Undigraph m){
-			Undigraph mat(n);
-			for(int i = 0; i < n; i++)
-				for(int j = 0; j < n; j++)
-					for(int k = 0; k < n; k++)
-						if(at(i, k) && m.at(k, j)){
-							mat.set(i, j);
-							break;
-						}
-			return mat;
-		}
-
-		Undigraph operator^(Undigraph m){
-			Undigraph mat(n);
-			for(int i = 0; i < (1+n)*n/16+1; i++)
-				mat.matrix[i] = m.matrix[i] ^ matrix[i];
-			return mat;
-		}
-
-		Undigraph operator&(Undigraph m){
-			Undigraph mat(n);
-			for(int i = 0; i < (1+n)*n/16+1; i++)
-				mat.matrix[i] = m.matrix[i] & matrix[i];
-			return mat;
-		}
-
-		Undigraph& operator&=(Undigraph m){
-			*this = *this & m;
-			return *this;
-		}
-
-		Undigraph operator|(Undigraph m){
-			Undigraph mat(n);
-			for(int i = 0; i < (1+n)*n/16+1; i++)
-				mat.matrix[i] = m.matrix[i] | matrix[i];
-			return mat;
-		}
-
-		Undigraph operator|=(Undigraph m){
-			*this = *this | m;
-			return *this;
-		}
-
-		Undigraph& operator=(Undigraph m){
-			memcpy(matrix, m.matrix, (1+n)*n/16+1);
-			return *this;
-		}
-
-		Undigraph& operator*=(Undigraph m){
-			*this = *this * m;
-			return *this;
-		}
-
-		bool at(int x, int y){
-			if(x > y)
-				return at(y, x);
-			int index = y-x + (n+n-x+1)*x/2;
-			unsigned char byte = matrix[index/8];
-			return byte & 0x80>>index%8;
-		}
-
-		void set(int x, int y){
-			if(x > y)
-				return set(y, x);
-			int index = y-x + (n+n-x+1)*x/2;
-			matrix[index/8] = matrix[index/8] | 0x80>>index%8;
-			return;
-		}
-
-		void unset(int x, int y){
-			if(x > y)
-				return unset(y, x);
-			int index = y-x + (n+n-x+1)*x/2;
-			matrix[index/8] = matrix[index/8] & (0x80>>index%8^0xFF);
-			return;
-		}
-
-		bool empty(){
-			for(int i = 0; i < (1+n)*n/16+1; i++)
-				if(matrix[i])
-					return false;
-			return true;
-		}
-};
-
 int main(){
 	int n, m, p;
 	cin>>n>>m>>p;
-	Undigraph probe(n);
-	for(int i = 0; i < m; i++){
-		int a, b;
-		cin>>a>>b;
-		probe.set(a-1, b-1);
-	}
-	Undigraph known(n);
-	known = probe;
-	for(int i = 0; i < n; i++)
-		known.set(i, i);
-	Undigraph mask(n);
-	for(int i = 0; i < n; i++)
-		for(int j = i; j < n; j++)
-			mask.set(i, j);
+	short * map = new short[n] {0};
+	short ** parts = new short*[n+1];
+	for(int i = 0; i < n+1; i++)
+		parts[i] = new short[n+1]{0};
 
-	Undigraph step(n);
-	step = probe;
-	while(!probe.empty()){
-		probe *= step;
-		probe &= known^mask;
-		known |= probe;
+	for(int i = 0; i < m; i++){
+		short a, b;
+		cin>>a>>b;
+		if(map[a] == 0 && map[b] == 0){
+			short dest = 0;
+			for(int i = 1; i <= n; i++)
+				if(parts[i][0] == 0)
+					dest = i;
+			if(!dest)
+				exit(1);
+
+			map[a] = map[b] = dest;
+			parts[dest][ ++parts[dest][0] ] = a;
+			parts[dest][ ++parts[dest][0] ] = b;
+		}else if(map[a] == 0 || map[b] == 0){
+			short zero = map[a]==0 ? a : b;
+			short nonzero = map[a]==0 ? b : a;
+			map[zero] = map[nonzero];
+			short nzpart = map[nonzero];
+			parts[nzpart][ ++parts[nzpart][0] ] = zero;
+		}else{
+			short dest = a<b ? a : b;
+			short source = a<b ? b : a;
+			short srcpart = map[source];
+			short destpart = map[dest];
+			for(int i = 1; i <= parts[srcpart][0]; i++){
+				map[ parts[srcpart][i] ] = destpart;
+				parts[destpart][ ++parts[destpart][0] ] = 
+					parts[srcpart][i];
+			}
+			parts[srcpart][0] = 0;
+		}
 	}
 
 	for(int i = 0; i < p; i++){
-		int x, y;
-		cin>>x>>y;
-		if(known.at(x, y))
+		short a, b;
+		cin>>a, b;
+		if(map[a] == map[b] && map[a] != 0 && map[b] != 0)
 			cout<<"Yes"<<endl;
 		else
 			cout<<"No"<<endl;
 	}
-	probe.release();
-	step.release();
-	known.release();
-	mask.release();
+	return 0;
 }
